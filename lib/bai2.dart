7bd1a2075016d1267f890e58bfa4ddd685058523
Nguyen Trung Hieu <nguyentrunghieu300824@gmail.com>
2026-08-29 15:50:47 +0700

    Update lesson 02

--- lib/bai2.dart
+++ lib/bai2.dart
@@ -1,38 +1,60 @@
 import 'dart:io';
+import 'dart:math';
+
+// Kiểm tra số nguyên tố
+bool soNguyenTo(int value) {
+  if (value < 2) {
+    return false;
+  }
+  for (int i = 2; i < sqrt(value); i++) {
+    if (value % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
 void main() {
-  int? n;
-  while (true) {
-    stdout.write('Nhập vào một số nguyên dương (> 10): ');
-    n = int.tryParse(stdin.readLineSync() ?? '');
+  // Nhập 1 số nguyên dương lớn hơn 10
+  int soNguyen;
+  do {
+    stdout.write("Nhập một số nguyên lớn hơn 10: ");
+    soNguyen = int.tryParse(stdin.readLineSync()!) ?? 1;
+  } while (soNguyen <= 10);
 
-    if (n != null && n > 10) {
-      break;
-    }
-    print('Giá trị không hợp lệ! Vui lòng nhập số nguyên lớn hơn 10.\n');
-  }
-
-  String nStr = n.toString();
-  int soChuSo = nStr.length;
-
+  int temp = soNguyen;
+  int demChuSo = 0;
   int tongChuSo = 0;
   bool coChuSoLe = false;
-  int temp = n;
+  int chuSoLonNhat = 0;
+  List<int> list = [];
 
   while (temp > 0) {
     int chuSo = temp % 10;
+    demChuSo++;
     tongChuSo += chuSo;
-
     if (chuSo % 2 != 0) {
       coChuSoLe = true;
     }
-
+    if (chuSo > chuSoLonNhat) {
+      chuSoLonNhat = chuSo;
+    }
     temp ~/= 10;
   }
 
-  print('\n========== KẾT QUẢ ==========');
-  print('Số vừa nhập: $n');
-  print('a. Số này có $soChuSo chữ số.');
-  print('b. Tổng các chữ số: $tongChuSo');
-  print('c. Có chứa chữ số lẻ không?: ${coChuSoLe ? "CÓ" : "KHÔNG"}');
+  for (int i = 0; i <= soNguyen; i++) {
+    if (soNguyenTo(i)) {
+      list.add(i);
+    }
+  }
+
+  print("\nSố $soNguyen có: $demChuSo chữ số");
+  print("Tổng các chữ số: $tongChuSo");
+  print("Chứa chữ số lẻ: ${coChuSoLe ? "Có" : "Không"}");
+  print("Chữ số lớn nhất: $chuSoLonNhat");
+  if (list.isNotEmpty) {
+    print("Danh sách các số nguyên tố bên trong $soNguyen: $list");
+  } else {
+    print("Không có chữ số nguyên tố nào bên trong $soNguyen.");
+  }
 }
