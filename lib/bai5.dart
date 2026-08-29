36195751ab94eb5ffc4b4435669d9aadbbb776f1
Nguyen Trung Hieu <nguyentrunghieu300824@gmail.com>
2026-08-29 17:11:38 +0700

    Update lesson 5

--- lib/bai5.dart
+++ lib/bai5.dart
@@ -1,32 +1,38 @@
 import 'dart:io';
 
 void main() {
+  // Nhập vào một chuỗi
   stdout.write('Nhập vào một chuỗi: ');
   String input = stdin.readLineSync() ?? '';
+  print('\nChuỗi vừa nhập: $input');
 
-  print('\na. Chuỗi vừa nhập: $input');
-
+  // Chuỗi chứa các nguyên âm
   String vowels = 'aăâeêioôơuưyAĂÂEÊIOÔƠUƯY';
+
+  // Đếm số nguyên âm trong chuỗi
   int countVowels = 0;
   for (int i = 0; i < input.length; i++) {
     if (vowels.contains(input[i])) {
       countVowels++;
     }
   }
-  print('b. Số ký tự nguyên âm: $countVowels');
+  print('Số ký tự nguyên âm: $countVowels');
 
+  // Đếm số từ trong chuối
   List<String> words = input
       .trim()
       .split(RegExp(r'\s+'))
       .where((w) => w.isNotEmpty)
       .toList();
-  print('c. Số từ trong chuỗi: ${words.length}');
+  print('Số từ trong chuỗi: ${words.length}');
 
+  // Kiểm tra đối xứng
   String cleanStr = input.replaceAll(RegExp(r'\s+'), '').toLowerCase();
   String reversedStr = cleanStr.split('').reversed.join('');
   bool isSymmetric = cleanStr.isNotEmpty && cleanStr == reversedStr;
-  print('d. Chuỗi có đối xứng không: ${isSymmetric ? "CÓ" : "KHÔNG"}');
+  print('Chuỗi có đối xứng không: ${isSymmetric ? "CÓ" : "KHÔNG"}');
 
+  // Đảo ngược danh sách
   String reversedWords = words.reversed.join(' ');
-  print('e. Đảo ngược từ trong chuỗi: $reversedWords');
+  print('Đảo ngược từ trong chuỗi: $reversedWords');
 }
